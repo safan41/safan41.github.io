1cf33ed8a19891975bd75169eb4af17f6c539b33
Safwan Morshed <safan41@outlook.com>
2017-08-30 17:40:21 -0500

    Updated website
    I made it a website

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-function init() {
-  var mapOptions = {                                 // Set up the map options
-    center: new google.maps.LatLng(40.782710,-73.965310),
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    zoom: 13
-  };
-  var venueMap;                                      // Map() draws a map
-  venueMap = new google.maps.Map(document.getElementById('map'), mapOptions);
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18) {
+    greeting = 'evenin';
+} else if (hourNow > 12) {
+    greeting = 'Good afternoon!';
+} else if (hourNow > 0) {
+    greeting = 'Mornin';
+} else {
+    greeting = 'Welcome!';
 }
 
-function loadScript() {
-  var script = document.createElement('script');     // Create <script> element
-  script.src = 'http://maps.googleapis.com/maps/api/js?sensor=false&callback=init';
-  document.body.appendChild(script);                 // Add element to page
-}
-
-window.onload = loadScript;                          // on load call loadScript()
+document.write('<h3>' + greeting + '</h3>');
