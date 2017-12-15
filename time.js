1387651a8038f5ed47aa62503207b39a99f89c16
Safwan Morshed <safan25641@gmail.com>
2017-12-14 18:54:15 -0600

    BEAUTIFUL STARS

--- time.js
+++ time.js
@@ -6,13 +6,13 @@ function startTime() {
     m = checkTime(m);
     s = checkTime(s);
     document.getElementById('txt').innerHTML =
-    h + ":" + m + ":" + s;
+        h + ":" + m + ":" + s;
     var t = setTimeout(startTime, 500);
 }
+
 function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    if (i < 10) {
+        i = "0" + i
+    }; // add zero in front of numbers < 10
     return i;
 }
-
-  
-  
