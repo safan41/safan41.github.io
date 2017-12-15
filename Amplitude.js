845721976fd06ae7240dfa24d30a812bd3612a30
Safwan Morshed <safan25641@gmail.com>
2017-12-14 21:09:43 -0600

    SOUND WORKS YET?????

--- Amplitude.js
+++ Amplitude.js
@@ -6159,29 +6159,3 @@ module.exports = exports['default'];
 /***/ })
 /******/ ]);
 });
-Amplitude.init({
-    "songs": [
-        {
-            "name": "Clouds",
-            "artist": "Calex",
-            "album": "Clouds",
-            "url": "Clouds.mp3",
-            "cover_art_url": "https://i1.sndcdn.com/artworks-000230533812-9jddqw-t500x500.jpg"
-        },
-        {
-            "name": "Astronomia",
-            "artist": "Vicetone",
-            "album": "Astronomia",
-            "url": "Astronomia.mp3",
-            "cover_art_url": "https://i1.sndcdn.com/artworks-000084701476-j62fpy-t500x500.jpg"
-        },
-        {
-            "name": "Fire",
-            "artist": "Elektronomia",
-            "album": "NCS",
-            "url": "fire.mp3",
-            "cover_art_url": "https://i1.sndcdn.com/artworks-000237436599-fg29gu-t500x500.jpg"
-        }
-    ]
-});
-
